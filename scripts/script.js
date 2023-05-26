94b0432bdee7655a0ba8ce89a0867ee51276a096
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-05-26 13:45:17 +0200

    add social container + interactivity on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,24 @@ const imgShareContainer = document.querySelector('.media__share-container');
 const imgShareDefault = document.querySelector('.media__button__share');
 const imgShareWhite = document.querySelector('.media__button__share--white');
 
+const socialContainer = document.querySelector('.social__container');
+const socialShareContainer = document.querySelector('.social__share-container');
+
+const mediaContainer = document.querySelector('.media__container');
+
 imgShareContainer.addEventListener('mouseenter', () => {
   imgShareDefault.style.display = 'none';
   imgShareWhite.style.display = 'block';
+});
+
+imgShareContainer.addEventListener('click', () => {
+  mediaContainer.style.display = 'none';
+  socialContainer.style.display = 'flex';
+});
+
+socialShareContainer.addEventListener('click', () => {
+  mediaContainer.style.display = 'flex';
+  socialContainer.style.display = 'none';
 });
 
 imgShareContainer.addEventListener('mouseleave', () => {
