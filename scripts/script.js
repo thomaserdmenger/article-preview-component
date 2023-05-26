7ba80e537ad253ce829c7bc7b02aea182796538b
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-05-26 17:41:17 +0200

    add social container + interactivity on desktop

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,23 @@ const imgShareContainer = document.querySelector('.media__share-container');
 const imgShareDefault = document.querySelector('.media__button__share');
 const imgShareWhite = document.querySelector('.media__button__share--white');
 
-const socialContainer = document.querySelector('.social__container');
+const socialContainer = document.querySelector('.social__container-mobile');
 const socialShareContainer = document.querySelector('.social__share-container');
+const bubbleContainer = document.querySelector('.social__bubble-container');
 
 const mediaContainer = document.querySelector('.media__container');
+const mediaShareContainer = document.querySelector('.media__share-container--desktop');
+const mediaShareDefault = document.querySelector('.media__button__share-desktop');
+const mediaShareWhite = document.querySelector('.media__button__share--white-desktop');
 
 imgShareContainer.addEventListener('mouseenter', () => {
   imgShareDefault.style.display = 'none';
   imgShareWhite.style.display = 'block';
+});
+
+mediaShareContainer.addEventListener('mouseenter', () => {
+  mediaShareDefault.style.display = 'none';
+  mediaShareWhite.style.display = 'block';
 });
 
 imgShareContainer.addEventListener('click', () => {
@@ -27,4 +36,17 @@ socialShareContainer.addEventListener('click', () => {
 imgShareContainer.addEventListener('mouseleave', () => {
   imgShareDefault.style.display = 'block';
   imgShareWhite.style.display = 'none';
+});
+
+mediaShareContainer.addEventListener('mouseleave', () => {
+  mediaShareDefault.style.display = 'block';
+  mediaShareWhite.style.display = 'none';
+});
+
+mediaShareContainer.addEventListener('click', () => {
+  if (bubbleContainer.style.display === 'block') {
+    bubbleContainer.style.display = 'none';
+  } else {
+    bubbleContainer.style.display = 'block';
+  }
 });
